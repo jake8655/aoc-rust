95e047f6c584149a00284b850209659a2984463c
Jake <dominik8655@gmail.com>
2024-01-25 20:35:18 +0100

    solve: day3 part1

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -1,61 +1,90 @@
 fn main() {
-    // let input = include_str!("./input");
-    let input = "467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..";
+    let input = include_str!("./input");
+    //     let input = "467..114..
+    // ...*......
+    // ..35..633.
+    // ......#...
+    // 617*......
+    // .....+.58.
+    // ..592.....
+    // ......755.
+    // ...$.*....
+    // .664.598..";
     let ouput = solve(input);
 
     dbg!(ouput);
 }
 
 fn solve(input: &str) -> u32 {
-    let matrix: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut matrix: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
-    let _ = matrix
-        .iter()
-        .enumerate()
-        .map(|(row, line)| {
-            line.iter().enumerate().for_each(|(col, character)| {
-                if character == &'.' || character.is_ascii_digit() {
-                    return;
-                }
+    let result: u32 = (0..matrix.len())
+        .map(|y| {
+            (0..matrix[y].len())
+                .map(|x| {
+                    if matrix[y][x] == '.' || matrix[y][x].is_ascii_digit() {
+                        return 0;
+                    }
 
-                println!("Found symbol {character} at col: {col} row: {row}");
+                    let mut numbers = Vec::new();
 
-                // Check top bottom left right and diagonal
-                let top = if row == 0 {
-                    None
-                } else {
-                    Some(matrix[row - 1][col])
-                };
-                let bottom = if row == matrix.len() - 1 {
-                    None
-                } else {
-                    Some(matrix[row + 1][col])
-                };
-                let left = if col == 0 {
-                    None
-                } else {
-                    Some(matrix[row][col - 1])
-                };
-                let right = if col == matrix[row].len() - 1 {
-                    None
-                } else {
-                    Some(matrix[row][col + 1])
-                };
+                    // Check if the character to the top, bottom, left, right, diagonals exists and is a number
+                    // If it is, walk the number to the beginning read it from there
+                    if y != 0 && matrix[y - 1][x].is_ascii_digit() {
+                        numbers.push(get_number_from_middle(&mut matrix, x, y - 1));
+                    }
+                    if y != matrix.len() - 1 && matrix[y + 1][x].is_ascii_digit() {
+                        numbers.push(get_number_from_middle(&mut matrix, x, y + 1));
+                    }
+                    if x != 0 && matrix[y][x - 1].is_ascii_digit() {
+                        numbers.push(get_number_from_middle(&mut matrix, x - 1, y));
+                    };
+                    if x != matrix[y].len() - 1 && matrix[y][x + 1].is_ascii_digit() {
+                        numbers.push(get_number_from_middle(&mut matrix, x + 1, y));
+                    };
 
-                // println!("right: {:?}", right);
-            })
+                    if y != 0 && x != 0 && matrix[y - 1][x - 1].is_ascii_digit() {
+                        numbers.push(get_number_from_middle(&mut matrix, x - 1, y - 1));
+                    };
+                    if y != 0 && x != matrix[y].len() - 1 && matrix[y - 1][x + 1].is_ascii_digit() {
+                        numbers.push(get_number_from_middle(&mut matrix, x + 1, y - 1));
+                    };
+                    if y != matrix.len() - 1 && x != 0 && matrix[y + 1][x - 1].is_ascii_digit() {
+                        numbers.push(get_number_from_middle(&mut matrix, x - 1, y + 1));
+                    };
+                    if y != matrix.len() - 1
+                        && x != matrix[y].len() - 1
+                        && matrix[y + 1][x + 1].is_ascii_digit()
+                    {
+                        numbers.push(get_number_from_middle(&mut matrix, x + 1, y + 1));
+                    };
+
+                    numbers.iter().sum::<u32>()
+                })
+                .sum::<u32>()
         })
-        .collect::<Vec<_>>();
-    12
+        .sum::<u32>();
+
+    result
+}
+
+fn get_number_from_middle(matrix: &mut [Vec<char>], mut x: usize, y: usize) -> u32 {
+    let mut str_number = String::new();
+
+    while x >= 1 && matrix[y][x - 1].is_ascii_digit() {
+        x -= 1;
+        if x == 0 {
+            break;
+        }
+    }
+
+    while x < matrix[y].len() && matrix[y][x].is_ascii_digit() {
+        str_number.push(matrix[y][x]);
+        matrix[y][x] = '.';
+        x += 1;
+    }
+
+    str_number.parse::<u32>().unwrap()
 }
 
 #[cfg(test)]
